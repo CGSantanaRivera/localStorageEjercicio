304590b3a86c0e8a2a2aae93fc4015d8c6ec1efd
Carlos Santana <carlosgabriel46@gmail.com>
2024-09-04 17:50:55 -0600

    Se agrega welcome.js y se modifican main.js, welcome.html, main.js y index.html

---
+++ Js/main.js
@@ -0,0 +1,9 @@
+const txtNombre = document.getElementById("txtNombre")
+const btnGuardar = document.getElementById("btnGuardar")
+
+btnGuardar.addEventListener("click", function(event){
+    event.preventDefault();
+    if (txtNombre.ariaValueMax.length<3){
+
+    }//if
+})
